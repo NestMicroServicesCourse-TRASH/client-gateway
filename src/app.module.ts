693690f6f8e13341bf6d0dd49ccb3073d283b1a4
Emmanuel Silva <emanuelsilvadiaz03@gmail.com>
2024-04-30 14:09:34 -0500

    Refactoring order's dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,8 @@ import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [ProductsModule, OrdersModule],
   controllers: [],
   providers: [],
+  imports: [ProductsModule, OrdersModule],
 })
 export class AppModule { }
